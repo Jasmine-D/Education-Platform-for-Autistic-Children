bc2344d30785c3b8ef8480149c171edea62a3dd7
sophiedoremi <912049502@qq.com>
2021-06-13 15:21:20 +0800

    test

---
+++ backend/src/test/java/com/example/backend/controller/myWordSetControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.backend.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class myWordSetControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void uploadWordSet() {
+    }
+
+    @Test
+    public void deleteWordSet() {
+    }
+
+    @Test
+    public void myStarWordSet() {
+    }
+
+    @Test
+    public void myWordSet() {
+    }
+}
---
+++ backend/src/test/java/com/example/backend/controller/reportControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.backend.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class reportControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAllReport() {
+    }
+
+    @Test
+    public void getSomeReport() {
+    }
+
+    @Test
+    public void createReport() {
+    }
+
+    @Test
+    public void modifyReport() {
+    }
+}
